ce6893dc672b0980ba3e08c6a6013db89b605de9
ankittayde1987 <anx9890@gmail.com>
2025-01-08 15:10:08 +0530

    initial commit

---
+++ DemoLibrary/Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DemoLibrary",
+    platforms: [
+        .iOS(.v16) // iOS version target
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DemoLibrary",
+            targets: ["DeviceTargetedDemoSDK"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(name: "DeviceTargetedDemoSDK", path: "./Sources/DeviceTargetedDemoSDK.xcframework"),
+
+    ]
+)
---
+++ DemoLibrary/Sources/DeviceTargetedDemoSDK.xcframework/ios-arm64/DeviceTargetedDemoSDK.framework/Headers/DeviceTargetedDemoSDK.h
@@ -0,0 +1,18 @@
+//
+//  DeviceTargetedDemoSDK.h
+//  DeviceTargetedDemoSDK
+//
+//  Created by Acclivis on 08/01/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeviceTargetedDemoSDK.
+FOUNDATION_EXPORT double DeviceTargetedDemoSDKVersionNumber;
+
+//! Project version string for DeviceTargetedDemoSDK.
+FOUNDATION_EXPORT const unsigned char DeviceTargetedDemoSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceTargetedDemoSDK/PublicHeader.h>
+
+
