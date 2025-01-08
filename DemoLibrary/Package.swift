c1bacadcdb044b71d30c9bba4628371deb735132
ankittayde1987 <anx9890@gmail.com>
2025-01-08 15:46:05 +0530

    version updated

--- DemoLibrary/Package.swift
+++ DemoLibrary/Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "DemoLibrary",
     platforms: [
-        .iOS(.v16) // iOS version target
+        .iOS(.v15) // iOS version target
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
